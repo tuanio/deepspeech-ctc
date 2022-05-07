68ee2bb35cb5d651fe9c6a3b1e9e840e3f4f66a2
tuanio <nvatuan3@gmail.com>
2022-05-07 19:04:49 +0700

    add files:

--- utils.py
+++ utils.py
@@ -24,8 +24,8 @@ class TextProcess:
         self.vocab = self.origin_vocab[lang]
         self.list_vocab = self.origin_list_vocab[lang]
 
-    def text2int(self, s: str):
+    def text2int(self, s: str) -> torch.Tensor:
         return torch.Tensor([self.vocab[i] for i in s.lower()])
 
-    def int2text(self, s: torch.Tensor):
+    def int2text(self, s: torch.Tensor) -> str:
         return "".join([self.list_vocab[i] for i in s if i > 2])
